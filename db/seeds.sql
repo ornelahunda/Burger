fa28186293044cd5f0c6c5020c0a3b9ff1abb6b7
ornelahunda <ornelahunda@gmail.com>
2018-05-14 10:06:01 -0400

    creating the database an d populating it with values

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name, devour) VALUES ('The Phoenix Burger', false);
+INSERT INTO burgers (burger_name, devour) VALUES ('The Looney Burger', false);
+INSERT INTO burgers (burger_name, devour) VALUES ('Burgermeister Burger', false);
+INSERT INTO burgers (burger_name, devour) VALUES ('The Silver Fox Burger', false);
+INSERT INTO burgers (burger_name, devour) VALUES ('Bardot Burger', false);
