d3dd06807999ed9f3f0fe4638e5c315ba6136ca1
ornelahunda <ornelahunda@gmail.com>
2018-05-14 10:11:36 -0400

    creating a connection file to connect to the database

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// Setting up the connection with the database 
+
+// Dependencies:
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
